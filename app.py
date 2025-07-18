266c5bca4f936634093584e67e0cafed1f62ba86
Andrew Bell <andrew.bell@kyndryl.com>
2025-07-18 11:03:15 +0100

    webhook test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 menu_items = [
-    {'id': 1, 'name': 'Margherita Pizza', 'description': 'Classic cheese and tomato pizza.', 'price': 8.5},
+    {'id': 1, 'name': 'Margherita Pizzas', 'description': 'Classic cheese and tomato pizza.', 'price': 8.5},
     {'id': 2, 'name': 'Veggie Burger', 'description': 'Grilled veggie patty with lettuce and tomato.', 'price': 7.0},
     {'id': 3, 'name': 'Caesar Salad', 'description': 'Crisp romaine with Caesar dressing.', 'price': 6.5}
 ]
